899b2c7fd68e681be4f57b044fa9dd3a7ffbe415
asam guo <asamguo@asamdeMacBook-Pro.local>
2024-07-06 13:59:38 -0700

    Lab1

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,10 @@
+import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
+
+export default function Kanbas() {
+  return (
+    <div id="wd-kanbas">
+      <KanbasNavigation />
+      <Dashboard />
+    </div>
+);}
