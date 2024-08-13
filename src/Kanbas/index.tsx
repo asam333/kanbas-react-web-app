b0d1a096e9efff72d73e4ba7f25b29b9bd3e7e78
asam guo <asamguo@asamdeMacBook-Pro.local>
2024-08-12 20:47:54 -0700

    a6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -42,7 +42,7 @@ export default function Kanbas() {
   };
   return (
     <Provider store={store}>
-      <Session>
+      {/* <Session> */}
         <div id="wd-kanbas" className="h-100">
           <KanbasNavigation />
           <div className="wd-main-content-offset p-3">
@@ -50,7 +50,7 @@ export default function Kanbas() {
               <Route path="/" element={<Navigate to="Dashboard" />} />
               <Route path="Account/*" element={<Account />} />
               <Route path="Dashboard" element={
-                <ProtectedRoute>
+                // <ProtectedRoute>
                   <Dashboard
                     courses={courses}
                     course={course}
@@ -58,19 +58,19 @@ export default function Kanbas() {
                     addNewCourse={addNewCourse}
                     deleteCourse={deleteCourse}
                     updateCourse={updateCourse} />
-                </ProtectedRoute>
+                // </ProtectedRoute> 
               } />
               <Route path="Courses/:cid/*" element={
-                <ProtectedRoute>
+                // <ProtectedRoute>
                   <Courses courses={courses} />
-                </ProtectedRoute>
+                // </ProtectedRoute>
                 }/>
               <Route path="Calendar" element={<h1>Calendar</h1>} />
               <Route path="Inbox" element={<h1>Inbox</h1>} />
             </Routes>
           </div>
         </div>
-      </Session>
+      {/* </Session> */}
     </Provider>
   );
 }
